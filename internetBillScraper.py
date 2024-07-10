ca85b377d2ed6d6f2549dc4ff5d0ff2091254402
arsalanKhawar <ak78@njit.edu>
2024-07-10 15:34:12 -0400

    added comments. code can login to optimum account successfully

--- internetBillScraper.py
+++ internetBillScraper.py
@@ -4,17 +4,26 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
-
+#open json file
 with open('props.json') as f:
     props = json.load(f)
+
+#configure webdriver
 driver = webdriver.Chrome()
 driver.get("https://www.optimum.net/")
+
+#get credentials from props file
 optimumID = props['optimumID']
 password = props['optimumPassword']
+
+#initialize optimumid and password fields
 idField = driver.find_element(By.ID,"homeLoginFormOptimumId")
 passwordField = driver.find_element(By.ID, "homeLoginFormPassword")
+
+#input credentials and press ENTER
 idField.send_keys(optimumID)
 passwordField.send_keys(password)
 passwordField.send_keys(Keys.RETURN)
+
 time.sleep(15)
 driver.quit()
